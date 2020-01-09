d09dccc1469381c62c13d322f2fe65c0a168d8b6
dlieber <dick@u505.com>
2020-01-09 14:36:42 -0600

    Use typesafeconfig-guice to allow injecting config values.

--- app/Module.scala
+++ app/Module.scala
@@ -4,6 +4,7 @@ import java.nio.file.Paths
 import akka.actor.{ActorRef, ActorSystem}
 import be.objectify.deadbolt.scala.cache.HandlerCache
 import be.objectify.deadbolt.scala.filters.AuthorizedRoutes
+import com.github.racc.tscg.TypesafeConfigModule
 import com.google.inject.{AbstractModule, Provides}
 import com.typesafe.config.Config
 import javax.inject.Singleton
@@ -12,9 +13,12 @@ import org.wa9nnn.wfdserver.Loader
 import org.wa9nnn.wfdserver.actor.BulkLoaderActorAnno
 import org.wa9nnn.wfdserver.auth.{CredentialsDao, WFDAuthorizedRoutes, WfdHandlerCache}
 import org.wa9nnn.wfdserver.contest.SubmissionControlDao
+import play.api.{Configuration, Environment}
 
-class Module extends AbstractModule with ScalaModule {
+class Module(environment: Environment, configuration: Configuration) extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
+    install(TypesafeConfigModule.fromConfigWithPackage(configuration.underlying, "org.wa9nnn.wfdserver"))
+
     bind[AuthorizedRoutes].to[WFDAuthorizedRoutes]
     bind[HandlerCache].to[WfdHandlerCache]
   }
